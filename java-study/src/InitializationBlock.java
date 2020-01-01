dd5e9b5412e18780f0ddeec5e18c1d12c08f7c61
Droomii <dowoo2594@naver.com>
2020-01-01 23:45:38 +0900

    initialization block study

---
+++ java-study/src/InitializationBlock.java
@@ -0,0 +1,24 @@
+
+public class InitializationBlock {
+
+	int val1;
+	char val2;
+	int[] values;
+
+	{
+		int[] values = new int[10];
+		for (int i = 0; i < values.length; i++) {
+			values[i] = 1;
+		}
+	}
+	
+	public InitializationBlock() {}
+
+	public InitializationBlock(int pVal1) {
+		this.val1 = pVal1;
+	}
+
+	public InitializationBlock(char pVal2) {
+		this.val2 = pVal2;
+	}
+}
